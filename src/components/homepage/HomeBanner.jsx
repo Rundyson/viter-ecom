ea478b8842a89745662268b5169350a0bcae9b0f
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 22:35:19 +0800

    Update HomeBanner.jsx

--- src/components/homepage/HomeBanner.jsx
+++ src/components/homepage/HomeBanner.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import { imgPath } from '../helpers/functions-general'
 import { Link } from 'react-router-dom'
 
-const HomeBanner = ({img, header, subheader}) => {
+const HomeBanner = ({ img, header, subheader }) => {
   return (
-    <>
-        <section className="home-banner relative pt-28 md:pt-32">
-            <div className="container">
-            <img src={`${imgPath}/${img}`} alt="" className="rounded-3xl h-screen md:h-full w-full object-cover"/>
-            <div className="banner-details absolute bottom-10 left-5 lg:left-20 text-white w-[400px] md:w-[1700px] md:mx-auto">
-                <h1 className="font-semibold mb-10">{header}</h1>
-                <div className=" flex flex-col items-start gap-5 md:flex md:flex-row md:justify-between">
-                <small>{subheader}</small>
-                <button className="bg-white text-black px-5 py-1 rounded-full text-sm hover:bg-black hover:text-white">
-                  <Link to="/buynow">Buy Now</Link> 
-                </button>
-                </div>
-            </div>
-            </div>
-        </section>
-    </>
+    <section className="home-banner relative pt-28 md:pt-32">
+      <div className="container mx-auto px-5 md:px-20">
+        <img
+          src={img}
+          alt=""
+          className="rounded-3xl w-full max-h-[80vh] object-cover"
+        />
+        <div className="banner-details absolute bottom-10 left-5 lg:left-20 text-white w-full md:w-[80%]">
+          <h1 className="font-semibold mb-4 md:mb-10 text-2xl md:text-5xl">{header}</h1>
+          <div className="flex flex-col md:flex-row md:justify-between items-start gap-3 md:gap-5">
+            <small className="text-sm md:text-base">{subheader}</small>
+            <button className="bg-white text-black px-5 py-2 rounded-full text-sm hover:bg-black hover:text-white mt-2 md:mt-0">
+              <Link to="/buynow">Buy Now</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
