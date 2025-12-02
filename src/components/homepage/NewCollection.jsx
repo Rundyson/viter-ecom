f831c80f08c6294de4d2e3a41da3b889786e6e2c
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 22:41:36 +0800

    Refactor NewCollection component layout and image source

--- src/components/homepage/NewCollection.jsx
+++ src/components/homepage/NewCollection.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { imgPath } from '../helpers/functions-general'
 
 const NewCollection = ({ item, key }) => {
   return (
     <section key={key} className="px-5 md:px-20 mb-10">
-      <div className="new-collection-card flex flex-col gap-3 md:gap-5 items-center">
+      <div className="flex flex-col items-center gap-4">
         <img
-          src={item.img}
+          src={`${imgPath}/${item.img}`}
           alt={item.title}
-          className="rounded-3xl w-full max-w-[400px] md:max-w-full max-h-[25rem] object-cover"
+          className="rounded-3xl w-full max-w-[400px] md:max-w-[500px] max-h-[400px] object-cover"
         />
-        <div className="collection-texts text-center md:text-left mt-2">
+        <div className="collection-texts text-center">
           <h6 className="font-semibold">{item.title}</h6>
           <small className="text-gray-500">{item.info}</small>
           <h6 className="font-semibold">${item.price}</h6>
