b3ddfaa9210e970714b04e64f864777584eac348
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 22:41:15 +0800

    Update HomeBanner.jsx

--- src/components/homepage/HomeBanner.jsx
+++ src/components/homepage/HomeBanner.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { imgPath } from '../helpers/functions-general'
 import { Link } from 'react-router-dom'
 
 const HomeBanner = ({ img, header, subheader }) => {
@@ -6,15 +7,15 @@ const HomeBanner = ({ img, header, subheader }) => {
     <section className="home-banner relative pt-28 md:pt-32">
       <div className="container mx-auto px-5 md:px-20">
         <img
-          src={img}
+          src={`${imgPath}/${img}`}
           alt=""
-          className="rounded-3xl w-full max-h-[80vh] object-cover"
+          className="rounded-3xl w-full max-w-[1200px] max-h-[500px] object-cover mx-auto"
         />
-        <div className="banner-details absolute bottom-10 left-5 lg:left-20 text-white w-full md:w-[80%]">
-          <h1 className="font-semibold mb-4 md:mb-10 text-2xl md:text-5xl">{header}</h1>
-          <div className="flex flex-col md:flex-row md:justify-between items-start gap-3 md:gap-5">
+        <div className="banner-details absolute bottom-5 left-5 lg:left-20 text-white w-full md:w-[80%]">
+          <h1 className="font-semibold mb-2 md:mb-5 text-xl md:text-4xl">{header}</h1>
+          <div className="flex flex-col md:flex-row md:justify-between items-start gap-2 md:gap-4">
             <small className="text-sm md:text-base">{subheader}</small>
-            <button className="bg-white text-black px-5 py-2 rounded-full text-sm hover:bg-black hover:text-white mt-2 md:mt-0">
+            <button className="bg-white text-black px-4 py-2 rounded-full text-sm hover:bg-black hover:text-white mt-2 md:mt-0">
               <Link to="/buynow">Buy Now</Link>
             </button>
           </div>
