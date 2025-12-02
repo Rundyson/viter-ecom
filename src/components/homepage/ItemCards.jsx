e8b38029aa1db5c4225c8e8ee847122ce25f4a50
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 22:18:14 +0800

    Update ItemCards.jsx

--- src/components/homepage/ItemCards.jsx
+++ src/components/homepage/ItemCards.jsx
@@ -5,37 +5,37 @@ import { Link } from 'react-router-dom'
 const ItemCards = () => {
     const itemCardsArray = [
         {
-            img: "card-1.png",
+            img: "card-1.jpg",
             title: "Loro Piana",
             info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.", 
             price: "45",
         },
         {
-            img: "card-2.png",
+            img: "card-2.jpg",
             title: "White Pants",
             info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.", 
             price: "90",
         },
         {
-            img: "card-3.png",
+            img: "card-3.jpg",
             title: "Bidha Glasses",
             info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.", 
             price: "50",
         },
         {
-            img: "card-4.png",
+            img: "card-4.jpg",
             title: "Brown Bomber",
             info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.", 
             price: "52",
         },
         {
-            img: "card-5.png",
+            img: "card-5.jpg",
             title: "Leather Shoes Jack",
             info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.", 
             price: "89",
         },
         {
-            img: "card-6.png",
+            img: "card-6.jpg",
             title: "Grey Tshirt",
             info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.", 
             price: "21",
