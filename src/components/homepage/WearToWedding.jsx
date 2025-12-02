3b072d8b78e7559d37d8183334cbb9925597704c
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 22:42:37 +0800

    Update WearToWedding.jsx

--- src/components/homepage/WearToWedding.jsx
+++ src/components/homepage/WearToWedding.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import { imgPath } from '../helpers/functions-general'
 
 const WearToWedding = ({ img, header, subheader, button }) => {
   return (
     <section className="py-10 px-5 md:px-20">
-      <div className="container mx-auto flex flex-col items-center">
+      <div className="flex flex-col items-center">
         <img
-          src={img}
+          src={`${imgPath}/${img}`}
           alt={header}
-          className="rounded-3xl mb-5 w-full max-h-[40rem] object-cover"
+          className="rounded-3xl w-full max-w-[600px] md:max-w-[800px] max-h-[400px] object-cover"
         />
-        <div className="wedding-texts flex flex-col items-center gap-3 text-center">
-          <h2 className="text-2xl md:text-4xl font-semibold">{header}</h2>
+        <div className="wedding-texts flex flex-col items-center gap-3 text-center mt-4">
+          <h2 className="text-xl md:text-3xl font-semibold">{header}</h2>
           <small className="text-gray-600">{subheader}</small>
-          <button className="bg-black text-white px-5 py-2 rounded-full hover:bg-white hover:text-black hover:border hover:border-black mt-2">
+          <button className="bg-black text-white px-4 py-2 rounded-full hover:bg-white hover:text-black hover:border hover:border-black mt-2">
             {button}
           </button>
         </div>
