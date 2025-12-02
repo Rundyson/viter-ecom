8be6c9cbac12a47dc0fb95fc97b5de136410e832
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 22:50:54 +0800

    Update Variation.jsx

--- src/components/homepage/Variation.jsx
+++ src/components/homepage/Variation.jsx
@@ -2,24 +2,16 @@ import React from 'react'
 import { imgPath } from '../helpers/functions-general'
 import { Link } from 'react-router-dom'
 
-const Variation = ({ item, key }) => {
+const Variation = ({item, key}) => {
   return (
-    <section key={key} className="py-10 flex justify-center px-5 md:px-20">
-      <div className="relative">
-        <img
-          src={`${imgPath}/${item.VariationImg}`}
-          alt={item.VariationTitle}
-          className="rounded-3xl w-full max-w-[480px] md:max-w-[600px] max-h-[400px] object-cover mx-auto"
-        />
-        <div className="absolute bottom-3 left-3 flex flex-col gap-1 text-white">
-          <h2 className="font-semibold text-sm md:text-lg">{item.VariationTitle}</h2>
-          <Link to="/seedetails">
-            <button className="bg-white text-black px-4 py-1 rounded-full text-xs md:text-sm hover:text-white hover:bg-black">
-              {item.VariationButton}
-            </button>
-          </Link>
-        </div>
-      </div>
+    <section className="py-10">
+            <div className="relative" key={key}>
+                <img className="rounded-3xl h-[20rem] w-[480px] md:w-full md:h-[30rem]" src={`${imgPath}/${item.VariationImg}`} alt="" />
+                <div className="absolute bottom-10 left-5 flex flex-col gap-2 text-white">
+                    <h2 className="font-semibold">{item.VariationTitle}</h2>
+                    <Link to="/seedetails"><button className="bg-white text-black px-5 py-1 rounded-3xl text-sm hover:text-white hover:bg-black">{item.VariationButton}</button></Link>
+                </div>
+            </div>
     </section>
   )
 }
