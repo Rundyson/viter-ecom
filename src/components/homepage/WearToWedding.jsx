4fce9a1a5df1c309955c61bc84d54a8e75a2df94
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 22:51:09 +0800

    Update WearToWedding.jsx

--- src/components/homepage/WearToWedding.jsx
+++ src/components/homepage/WearToWedding.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import { imgPath } from '../helpers/functions-general'
 
-const WearToWedding = ({ img, header, subheader, button }) => {
+const WearToWedding = ({img, header, subheader, button}) => {
   return (
-    <section className="py-10 px-5 md:px-20">
-      <div className="flex flex-col items-center">
-        <img
-          src={`${imgPath}/${img}`}
-          alt={header}
-          className="rounded-3xl w-full max-w-[600px] md:max-w-[800px] max-h-[400px] object-cover"
-        />
-        <div className="wedding-texts flex flex-col items-center gap-3 text-center mt-4">
-          <h2 className="text-xl md:text-3xl font-semibold">{header}</h2>
-          <small className="text-gray-600">{subheader}</small>
-          <button className="bg-black text-white px-4 py-2 rounded-full hover:bg-white hover:text-black hover:border hover:border-black mt-2">
-            {button}
-          </button>
+    <section className="py-10 justify-center">
+        <div className="container">
+            <img className="rounded-3xl mb-5 w-full" src={`${imgPath}/${img}`} alt="" />
+            <div className="wedding-texts flex flex-col items-center gap-5">
+                <h2>{header}</h2>
+                <small>{subheader}</small>
+                <button className="bg-black text-white px-5 py-1 rounded-full hover:bg-white hover:text-black hover:border-black hover:border">{button}</button>
+            </div>
         </div>
-      </div>
     </section>
   )
 }
