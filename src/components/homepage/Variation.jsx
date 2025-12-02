e3458d57bc20ec094d6ab68476d9b471a10887b8
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 22:42:19 +0800

    Enhance Variation component styling and image path

--- src/components/homepage/Variation.jsx
+++ src/components/homepage/Variation.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import { imgPath } from '../helpers/functions-general'
 import { Link } from 'react-router-dom'
 
 const Variation = ({ item, key }) => {
   return (
-    <section key={key} className="py-10 flex justify-center">
+    <section key={key} className="py-10 flex justify-center px-5 md:px-20">
       <div className="relative">
         <img
-          src={item.VariationImg}
+          src={`${imgPath}/${item.VariationImg}`}
           alt={item.VariationTitle}
-          className="rounded-3xl w-full max-w-[480px] md:max-w-full max-h-[30rem] object-cover mx-auto"
+          className="rounded-3xl w-full max-w-[480px] md:max-w-[600px] max-h-[400px] object-cover mx-auto"
         />
-        <div className="absolute bottom-5 left-5 flex flex-col gap-2 text-white">
-          <h2 className="font-semibold">{item.VariationTitle}</h2>
+        <div className="absolute bottom-3 left-3 flex flex-col gap-1 text-white">
+          <h2 className="font-semibold text-sm md:text-lg">{item.VariationTitle}</h2>
           <Link to="/seedetails">
-            <button className="bg-white text-black px-5 py-2 rounded-full text-sm hover:text-white hover:bg-black">
+            <button className="bg-white text-black px-4 py-1 rounded-full text-xs md:text-sm hover:text-white hover:bg-black">
               {item.VariationButton}
             </button>
           </Link>
