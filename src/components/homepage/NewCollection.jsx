90c7500a79ea878adf7ca4d2d3e10193e661254e
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 22:35:36 +0800

    Refactor NewCollection component layout and structure

--- src/components/homepage/NewCollection.jsx
+++ src/components/homepage/NewCollection.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
-import { imgPath } from '../helpers/functions-general'
 
-const NewCollection = ({item, key}) => {
+const NewCollection = ({ item, key }) => {
   return (
-    <>
-      <section>
-            <div className="new-collection-cards mb-5 items-center" key={key}>
-                <div className="flex flex-col gap-5">
-                    <img className="rounded-3xl h-[30rem] object-cover" src={`${imgPath}/${item.img}`} alt="" />
-                    <div className="collection-texts">
-                        <h6>{item.title}</h6>
-                        <small className="text-gray-500">{item.info}</small>
-                        <h6>${item.price}</h6>
-                    </div>
-                </div>
+    <section key={key} className="px-5 md:px-20 mb-10">
+      <div className="new-collection-card flex flex-col gap-3 md:gap-5 items-center">
+        <img
+          src={item.img}
+          alt={item.title}
+          className="rounded-3xl w-full max-w-[400px] md:max-w-full max-h-[25rem] object-cover"
+        />
+        <div className="collection-texts text-center md:text-left mt-2">
+          <h6 className="font-semibold">{item.title}</h6>
+          <small className="text-gray-500">{item.info}</small>
+          <h6 className="font-semibold">${item.price}</h6>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
 
